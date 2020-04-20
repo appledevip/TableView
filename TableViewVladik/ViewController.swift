538a293884b5136c672cd5ae86249c99ef5b9dd8
Stas Dashkevich <apple.devip@gmail.com>
2020-04-20 19:20:37 +0300

    Initial Commit

---
+++ TableViewVladik/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TableViewVladik
+//
+//  Created by Стас Дашкевич on 4/20/20.
+//  Copyright © 2020 Стас Дашкевич. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
